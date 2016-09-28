8b2f6dad9bc10cb10da18e0d849aed3d4514346d
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-28 16:57:52 -0400

    a6

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $headers = array(
     'Content-Type: application/json',
     'Authorization: Bearer '.$api_key
 );
-$client = new SendGrid\Client('https://api.sendgrid.com', $headers, '/v3/api_keys');
+$client = new SendGrid\Client('https://api.sendgrid.com', $headers, '/v3/scopes');
 
 // GET Collection
 $query_params = array('limit' => 100, 'offset' => 0);
