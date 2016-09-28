849b452a7c11d12f3bcdf5b225780c3a60c39176
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-28 17:14:15 -0400

    a6

--- index.php
+++ index.php
@@ -9,18 +9,39 @@ $path_to_config = dirname(__DIR__);
 $apiKey = getenv('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');
 
 $headers = array(
+    'Content-Type: application/json',
     'Authorization: Bearer '.$api_key
 );
-$client = new SendGrid\Client('https://api.sendgrid.com', $headers, '/v3');
+$client = new SendGrid\Client('https://api.sendgrid.com', $headers, '/v3', null);
 
 // GET Collection
 $query_params = array('limit' => 100, 'offset' => 0);
-$request_headers = array('api/mail.send.json');
+$request_headers = array('X-Mock: 200');
 $response = $client->api_keys()->get(null, $query_params, $request_headers);
 echo $response->statusCode();
 echo $response->body();
+echo $response->beaders();
+// POST
+$request_body = array(
+        'name' => 'My PHP API Key',
+        'scopes' => array(
+            'mail.send',
+            'alerts.create',
+            'alerts.read'
+        )
+);
+$response = $client->api_keys()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
 echo $response->headers();
+$response_body = json_decode($response->responseBody());
+$api_key_id = $response_body->api_key_id;
 
+// GET Single
+$response = $client->version('/v3')->api_keys()->_($api_key_id)->get();
+echo $response->statusCode();
+echo $response->body();
+echo $response->headers();
 ?> 
 
 <p>Next stuff</p>
