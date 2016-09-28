90af7e42a2ea77ec76d7123baefed563e49cdbf5
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-28 13:33:39 -0400

    a5

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ namespace SendGrid;
 // If you are using Composer
 require __DIR__ . '/vendor/autoload.php';
 
-print "Key=" . SENDGRID_API_KEY;
+print "Key=" . SENDGRID_API_KEY.value;
 /*
 try{
     $request_body = json_decode('{
