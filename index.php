c5242185855a45082a2232e045e1e924376ca20b
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-28 13:07:55 -0400

    a5

--- index.php
+++ index.php
@@ -3,15 +3,34 @@ namespace SendGrid;
 // If you are using Composer
 require __DIR__ . '<PATH_TO>/vendor/autoload.php';
 
+function helloEmail()
+{
+    $from = new Email(null, "test@example.com");
+    $subject = "Hello World from the SendGrid PHP Library";
+    $to = new Email(null, "test@example.com");
+    $content = new Content("text/plain", "some text here");
+    $mail = new Mail($from, $subject, $to, $content);
+    $to = new Email(null, "akbar.hirani123@gmail.com");
+    $mail->personalization[0]->addTo($to);
+    //echo json_encode($mail, JSON_PRETTY_PRINT), "\n";
+    return $mail;
+}
+/*
 try{
     $apiKey = getenv('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');
     $sg = new \SendGrid($apiKey);
+
+    $request_body = helloEmail();
+    $response = $sg->client->mail()->send()->post($request_body);
+    echo $response->statusCode();
+    echo $response->body();
+    echo $response->headers();
     
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 
-/*
+
 // GET Collection
 $query_params = ['limit' => 100, 'offset' => 0];
 $request_headers = ['X-Mock: 200'];
