b0955a5c3ecafa0b6dfca8173b1b1ee761292951
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-27 21:14:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+// If you are using Composer
+require 'vendor/autoload.php';
+
+function helloEmail()
+{
+    $from = new Email(null, "test@example.com");
+    $subject = "Hello World from the SendGrid PHP Library";
+    $to = new Email(null, "test@example.com");
+    $content = new Content("text/plain", "some text here");
+    $mail = new Mail($from, $subject, $to, $content);
+    $to = new Email(null, "akbar.hirani123@gmail.com");
+    $mail->personalization[0]->addTo($to);
+    //echo json_encode($mail, JSON_PRETTY_PRINT), "\n";
+    return $mail;
+}
+
+function sendHelloEmail()
+{
+    $apiKey = getenv('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');
+    $sg = new \SendGrid($apiKey);
+    $request_body = helloEmail();
+    $response = $sg->client->mail()->send()->post($request_body);
+    echo $response->statusCode();
+    echo $response->body();
+    echo $response->headers();
+}
+
+sendHelloEmail(); 
+?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -84,8 +114,8 @@ and open the template in the editor.
                 </nav>
             </div>
         </header>
-
-	<div class="container ptb">
+        
+        <div class="container ptb">
       		<div class="row">
         		<h2 class="centered mb">I craft handsome sites & stunning apps<br/>that empower your brand.</h2>
        	 		<div class="col-md-6">
