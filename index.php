717d5da5a331d0e7d1aa261881deb886ec27f97c
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-30 19:12:34 -0400

    a7

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if(!is_null($_POST['contactName'])) {
     $messageI = "" .$_POST['contactMessage'];
     
     sendEmailTo($sentFromN, $sentFromE, $messageI);
-    sendEmailFrom($sentFromN, $sentFromE, $messageI));
+    //sendEmailFrom($sentFromN, $sentFromE, $messageI));
 }
 
 /*
