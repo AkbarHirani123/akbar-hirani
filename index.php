c0904036593748705d3a211adcb8b01cf61dceb4
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-30 19:41:52 -0400

    a8

--- index.php
+++ index.php
@@ -32,10 +32,10 @@ function sendEmailTo( $sentFromName, $sentFromEmail, $messageIs ){
     $content = new SendGrid\Content("text/html", "<p>Hello ". $sentFromName ."!</p>
         <p style='text-indent:50px' >Thank you for contacting me, Akbar Hirani! Your message is very important and I will reach out to you ass soon as I can.</p>
         <p>The message you sent contained the following information:</p> 
-        <p><strong>Name: </strong>" . $sentFromName . "</p> 
+        <div style='padding: 25px;' ><p><strong>Name: </strong>" . $sentFromName . "</p> 
         <p><strong>Email: </strong><a href=mailto:" . $sentFromEmail .">". $sentFromEmail ."</a></p>
         <p><strong>Message was: </strong></p>
-        <p style='text-indent:50px' >". $messageIs ."</p><p>Note: This is an automated message. Please do not reply to this email.</p>");
+        <p>". $messageIs ."</p></div><p>Note: This is an automated message. Please do not reply to this email.</p>");
     $mail = new SendGrid\Mail($from, $subject, $to, $content);
     $mail->setTemplateId("3d828577-a0f9-4224-8ae7-6b29e04ac58d");
 
@@ -48,34 +48,6 @@ function sendEmailTo( $sentFromName, $sentFromEmail, $messageIs ){
     echo '<script>console.log("$response->statusCode()");</script>';
     echo '<script>console.log("$response->headers()");</script>';
     echo '<script>console.log("$response->body()");</script>';}*/
-
-}
-
-function sendEmailFrom( $sentFromName, $sentFromEmail, $messageIs ){
-    /*$from = new SendGrid\Email(null, "akbar-hirani-herokuapp@example.com");
-    $subject = "Receipt Email For Contacting Akbar Hirani"
-    $to = new SendGrid\Email(null, $sentFromEmail);
-    $content = new SendGrid\Content("text/html", "<p>Hello ". $sentFromName ."!</p>
-        <p style='text-indent:50px' >Thank you for contacting me, Akbar Hirani! Your message is very important and I will reach out to you ass soon as I can.</p>
-        <p>The message you sent contained the following information:</p> 
-        <p><strong>Name: </strong>" . $sentFromName . "</p> 
-        <p><strong>Email: </strong><a href=mailto:" . $sentFromEmail .">". $sentFromEmail ."</a></p>
-        <p><strong>Message was: </strong></p>
-        <p style='text-indent:50px' >". $messageIs ."</p><p>Note: This is an automated message. Please do not reply to this email.</p>");
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
-    $mail->setTemplateId("3d828577-a0f9-4224-8ae7-6b29e04ac58d");
-
-    $apiKey = getenv('SENDGRID_API_KEY');
-    $sg = new \SendGrid($apiKey);
-
-    try {
-        $response = $sg->client->mail()->send()->post($mail);
-    } catch (Exception $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
-    }
-    echo '<script>console.log("$response->statusCode()");</script>';
-    echo '<script>console.log("$response->headers()");</script>';
-    echo '<script>console.log("$response->body()");</script>';*/
 
 }
 
