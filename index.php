cca1fea36b3f32c745c8d0650fa0318e02ee85ce
Akbar Hirani <akbar.hirani123@gmail.com>
2016-09-30 18:29:02 -0400

    a7

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ function sendEmailTo( $sentFromName, $sentFromEmail, $messageIs ){
     echo '<script>console.log("$response->headers()");</script>';
     echo '<script>console.log("$response->body()");</script>';}
 
-function sendEmailTo( $sentFromName, $sentFromEmail, $messageIs ){
+function sendEmailFrom( $sentFromName, $sentFromEmail, $messageIs ){
     $from = new SendGrid\Email(null, "akbar-hirani-herokuapp@example.com");
     $subject = "Receipt Email For Contacting Akbar Hirani"
     $to = new SendGrid\Email(null, "" . $sentFromEmail);
