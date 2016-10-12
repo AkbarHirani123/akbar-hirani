e680d3909729fabb8707ee96db8f2134ca532e2d
Akbar Hirani <akbar.hirani123@gmail.com>
2016-10-12 15:01:11 -0400

    html5 video player skin change

--- videoplayer.js
+++ videoplayer.js
@@ -42,3 +42,97 @@ function clickedBar(e) {
 }
 
 window.addEventListener('load', doFirst, false);
+
+function another() {
+	barSize=360;
+	video = document.getElementById('video');
+
+	playB = document.getElementById('play-pause');
+	muteB = document.getElementById("mute");
+	full = document.getElementById('full-screen');
+
+	seekBar = document.getElementById('seek-bar');
+	volumeBar = document.getElementById('volume-bar');
+
+	playB.addEventListener("click", function() {
+		if(!video.paused && !video.ended) {
+			video.pause();
+			playB.innerHTML="Play";
+		}else {
+			video.play();
+			playB.innerHTML="Pause";
+		}
+	});
+	
+	muteB.addEventListener("click", function() {
+ 		if (video.muted == false) {
+		    video.muted = true;
+		    muteB.innerHTML = "Unmute";
+		} else {
+		    video.muted = false;
+		    muteB.innerHTML = "Mute";
+		}
+	});
+
+	full.addEventListener("click", function() {
+		if (video.webkitRequestFullscreen) {
+		    video.webkitRequestFullscreen(); // Chrome and Safari
+		}
+	});
+
+	seekBar.addEventListener("change", function() {
+		// Calculate the new time
+		var time = video.duration * (seekBar.value / 100);
+		// Update the video time
+		video.currentTime = time;
+	});
+
+	video.addEventListener("timeupdate", function() {
+		// Calculate the slider value
+		var value = (100 / video.duration) * video.currentTime;
+		// Update the slider value
+		seekBar.value = value;
+	});
+
+	// Pause the video when the slider handle is being dragged
+	seekBar.addEventListener("mousedown", function() {
+		video.pause();
+	});
+
+	// Play the video when the slider handle is dropped
+	seekBar.addEventListener("mouseup", function() {
+		video.play();
+	});
+
+	volumeBar.addEventListener("change", function() {
+	// Update the video volume
+		video.volume = volumeBar.value;
+	});
+
+	// KEY PRESS EVENTS
+
+	document.getElementById('video-container').addEventListener("keypress", keyPressHappened);
+}
+
+function keyPressHappened(e){
+	if(e.which==102){
+		if (video.webkitRequestFullscreen) {
+		    video.webkitRequestFullscreen(); // Chrome and Safari
+		}
+	}
+	console.log(e.which);
+}
+window.addEventListener('load', another, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
