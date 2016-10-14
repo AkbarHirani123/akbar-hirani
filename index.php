365cec8ee4b2c9c96724f9d09e986f5a43d9a221
Akbar Hirani <akbar.hirani123@gmail.com>
2016-10-14 12:39:27 -0400

    fixed file format. JS files in a separate folder.

--- index.php
+++ index.php
@@ -241,7 +241,7 @@ and open the template in the editor.
         <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>        
-        <script src="jquery-events.js"></script>
+        <script src="js/jquery-events.js"></script>
         <script>
         function myAjax(form) {
             var formData = $(form).serializeArray();
