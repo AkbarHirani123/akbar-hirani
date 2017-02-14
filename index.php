c2c45be2cb31eee7c9d472c194435f14b8252c10
Akbar Hirani <akbar.hirani123@gmail.com>
2017-02-13 20:58:38 -0500

    DB connect.

--- index.php
+++ index.php
@@ -75,6 +75,17 @@ function connect_pg_heroku() {
     return $db;
 }
 
+ 
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
 ?>
 <!DOCTYPE html>
 <!--
