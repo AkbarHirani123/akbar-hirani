39e701742c3d3024cdfaf70b5d4e1e623d42a06b
Akbar Hirani <akbar.hirani123@gmail.com>
2017-02-13 20:47:44 -0500

    DB connect.

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function pg_connection_string() {
 $db = pg_connect(pg_connection_string());
 if (!$db) {
     echo "Database connection error."
-    exit;
+    die();
 }
  
 # Now let's use the connection for something silly just to prove it works:
