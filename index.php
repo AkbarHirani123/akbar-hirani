4cd7c1b25cb3c831807ac74427be6ddc26af1884
Akbar Hirani <akbar.hirani123@gmail.com>
2017-02-13 20:56:27 -0500

    DB connect.

--- index.php
+++ index.php
@@ -66,29 +66,15 @@ echo $response->headers();
 */
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  // we will fill this out next
-	extract(parse_url($_ENV["DATABASE_URL"]));
-  	return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+function connect_pg_heroku() {
+   $db = pg_connect(getenv('CONNECTION_STRING'));
+    if (!$db) {
+        echo "Database connection error.";
+        die();
+    }
+    return $db;
 }
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-    echo "Database connection error."
-    die();
-}
- 
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
+
 ?>
 <!DOCTYPE html>
 <!--
