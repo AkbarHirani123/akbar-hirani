03d17dae3f66b9ecd29eb5a13ed5789b725b4c17
Akbar Hirani <akbar.hirani123@gmail.com>
2017-02-16 19:36:42 -0500

    Angular Directory Controller 1

--- angular/angularJS/directory.js
+++ angular/angularJS/directory.js
@@ -1,4 +1,19 @@
 angular.module('directoryApp', [])
+	.controller('directoryController', function() {
+		var dirList = this;
+		dirList.list = [
+			{name:'Akbar', age: 24},
+			{name:'Joe', age: 25},
+			{name:'Jacky', age: 23},
+			{name:'Bell', age: 21}
+		];
+
+		dirList.addPerson = function() {
+			dirList.list.push({name: dirList.name, age: dirList.age});
+			dirList.name='';
+			dirList.age=null;
+		};
+	/* INCORRECT PRACTICE TO USE SCOPE
 	.controller('directoryController', function($scope) {
 		$scope.list = [
 			{name:'Akbar', age: 24},
@@ -11,5 +26,5 @@ angular.module('directoryApp', [])
 			$scope.list.push({name: $scope.name, age: $scope.age});
 			$scope.name='';
 			$scope.age=null;
-		};
+		};*/
 	});
